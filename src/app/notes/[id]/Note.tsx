627c175e14e613cbab5b8c0a83a7d9b8b78830e0
UYEONG <uyeong21c@gmail.com>
2023-07-24 14:24:06 +0900

    Create a note edit page

--- src/app/notes/[id]/Note.tsx
+++ src/app/notes/[id]/Note.tsx
@@ -30,7 +30,7 @@ const Note: FC<Props> = ({ item }) => {
       <h3 className="text-pink-500 text-lg md:text-xl font-semibold break-all">{item.title}</h3>
       <p className="text-gray-500 break-all">{item.body}</p>
       <div className="flex flex-col sm:flex-row sm:justify-end gap-2.5">
-        <Link href={`/notes/${item.id}`} className="inline-block bg-pink-500 hover:bg-pink-600 active:bg-pink-700 focus-visible:ring ring-pink-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-2">Edit</Link>
+        <Link href={`/notes/${item.id}/edit`} className="inline-block bg-pink-500 hover:bg-pink-600 active:bg-pink-700 focus-visible:ring ring-pink-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-2">Edit</Link>
         <button onClick={deleteNote} className="inline-block bg-gray-200 hover:bg-gray-300 focus-visible:ring ring-pink-300 text-red-500 active:text-red-700 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-2">Delete</button>
       </div>
     </div>
--- src/app/notes/[id]/edit/EditNote.tsx
+++ src/app/notes/[id]/edit/EditNote.tsx
@@ -1,28 +1,33 @@
 'use client'
+
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { FC, useCallback, useState } from "react";
-import { z } from "zod";
 
-const NewNote: FC = () => {
+import { Note } from "../../type";
+
+type Props = {
+  item: Note;
+}
+
+const EditNote: FC<Props> = ({ item }) => {
   const router = useRouter();
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
-  const createNote = useCallback(async () => {
-    const res = await fetch(`/api/notes`, {
-      method: 'POST',
+  const [title, setTitle] = useState(item.title);
+  const [body, setBody] = useState(item.body);
+  const updateNote = useCallback(async () => {
+    const res = await fetch(`/api/notes/${item.id}`, {
+      method: 'PUT',
       body: JSON.stringify({ title, body }),
-      headers: { 'Content-Type': 'application/json' }
+      headers: { 'Content-Type': 'application/json' },
     });
     if (res.ok) {
-      const id = z.number().parse(await res.json());
-      alert('Note created');
-      router.push(`/notes`);
+      alert('Note updated');
+      router.push(`/notes/${item.id}`);
       router.refresh();
     } else {
-      alert('Note failed to create');
+      alert('Note failed to update');
     }
-  }, [router, title, body]);
+  }, [body, item.id, router, title]);
   return (
     <div className="flex flex-col bg-gray-100 rounded-lg relative p-5 gap-2.5">
       <div className="sm:col-span-2">
@@ -44,11 +49,11 @@ const NewNote: FC = () => {
         ></textarea>
       </div>
       <div className="flex flex-col sm:flex-row sm:justify-end gap-2.5">
-        <Link href={`/notes`} className="inline-block bg-gray-200 hover:bg-gray-300 focus-visible:ring ring-pink-300 text-gray-500 active:text-gray-700 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-2">Cancel</Link>
-        <button onClick={createNote} className="inline-block bg-pink-500 hover:bg-pink-600 active:bg-pink-700 focus-visible:ring ring-pink-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-2">Create</button>
+        <Link href={`/notes/${item.id}`} className="inline-block bg-gray-200 hover:bg-gray-300 focus-visible:ring ring-pink-300 text-gray-500 active:text-gray-700 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-2">Cancel</Link>
+        <button onClick={updateNote} className="inline-block bg-pink-500 hover:bg-pink-600 active:bg-pink-700 focus-visible:ring ring-pink-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-2">Save</button>
       </div>
     </div>
   );
 }
 
-export default NewNote;
+export default EditNote;
